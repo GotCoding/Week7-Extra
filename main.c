1141e4474c62386ebb4f8f0b0cef7a5d1807cb1d
GotCoding <156408514+GotCoding@users.noreply.github.com>
2024-02-23 08:01:08 +0000

    eiei

--- main.c
+++ main.c
@@ -15,30 +15,33 @@ int main(int argc , char **argv) {
   q.size=0;
   int i,x;
   
-  enqueue_struct(&q, 2);
-  enqueue_struct(&q, 3);
-  enqueue_struct(&q, 4);
-  enqueue_struct(&q, 5);
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-  printf("%d\n", dequeue_struct(&q));
-//  for(i=1;i<argc;i++){
-//         if(strcmp(argv[i],"x")==0){
-//             x=dequeue(&headPtr,&tailPtr);
-//             printf("dequeing %d\n",x);
-//         }
-//         else {
-//        enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
+  // enqueue_struct(&q, 2);
+  // enqueue_struct(&q, 3);
+  // enqueue_struct(&q, 4);
+  // enqueue_struct(&q, 5);
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+  // printf("%d\n", dequeue_struct(&q));
+ for(i=1;i<argc;i++){
+        if(strcmp(argv[i],"x")==0){
+            x=dequeue_struct(&q);
+            if (x != 0)
+              printf("dequeing %d\n",x);
+            else
+              printf("Empty queue\n");
+        }
+        else {
+       enqueue_struct(&q, atoi(argv[i]));
            
-//         }
-//  }
+        }
+ }
   return 0;
 }
