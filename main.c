d983014f55ce2bfa2f2926fb24c21eb1537a6368
GotCoding <156408514+GotCoding@users.noreply.github.com>
2024-02-23 23:02:15 +0700

    Update main.c

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 #include "Queue.h"
 
 int main(int argc , char **argv) {
-  NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
+  NodePtr headPtr = NULL;
+  NodePtr tailPtr = NULL;
 /* For struct Queue*/
   Queue  q;
-  q. headPtr=NULL;
-  q.tailPtr=NULL;
-  q.size=0;
-  int i,x;
+  q.headPtr = NULL;
+  q.tailPtr = NULL;
+  q.size = 0;
+  int i, x;
   
   // enqueue_struct(&q, 2);
   // enqueue_struct(&q, 3);
@@ -30,18 +30,21 @@ int main(int argc , char **argv) {
   // printf("%d\n", dequeue_struct(&q));
   // printf("%d\n", dequeue_struct(&q));
   // printf("%d\n", dequeue_struct(&q));
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue_struct(&q);
-            if (x != 0)
-              printf("dequeing %d\n",x);
-            else
-              printf("Empty queue\n");
-        }
-        else {
-       enqueue_struct(&q, atoi(argv[i]));
-           
-        }
+ for(i = 1; i < argc; i++)
+ {
+  if(strcmp(argv[i], "x") == 0)
+  {
+    x = dequeue_struct(&q);
+    if (x == 0)
+      printf("Empty queue\n");
+  }
+  else 
+  {
+    enqueue_struct(&q, atoi(argv[i]), atoi(argv[i+1]));
+    i += 1; //
+  }
  }
+ printf("================================================\n");
+ printf("There are %d people left in the queue\n", q.size);
   return 0;
 }
